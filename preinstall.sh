a3c3279b1f8d1d7e81391d1c4d23d1f37651c3f4
ZeroC0D3 Team <zeroc0d3.0912@gmail.com>
2017-12-28 19:08:34 +0700

    Add preinstall script

---
+++ preinstall.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+# ========== PREINSTALLATION SCRIPTS ========== # 
+#  Do Not Remove This File !                    #
+#  This is customize script before server up    # 
+# ============================================= #
